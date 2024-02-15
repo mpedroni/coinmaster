111ec548c0802e63de56bd7614f054a6c41b7fb7
Matheus Pedroni <mpedroni77@gmail.com>
2024-02-14 23:12:58 -0300

    first commit :tada:

---
+++ src/main/java/com/mpedroni/coinmaster/CoinmasterApplication.java
@@ -0,0 +1,13 @@
+package com.mpedroni.coinmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinmasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpedroni/coinmaster/CoinmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpedroni.coinmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
